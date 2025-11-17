cd1e3ae64c9983c54dea3f23654878d2ddad7fe3
Hemanth Vasu <hemanthvasu41468@gmail.com>
2025-11-18 01:21:21 +0530

    Change API URL from localhost to production

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -9,7 +9,7 @@ import ListSong from './pages/ListSong';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
-export const url = 'http://localhost:4000';
+export const url = 'https://tune-out-backend.onrender.com';
 
 const App = () => {
   return (
